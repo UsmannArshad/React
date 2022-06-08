d46139d609043209659dd491dcf1c19a97f833d0
UsmannArshad <usmanarshadbhatti480@gmail.com>
2022-06-08 18:23:45 +0500

    hello world printed

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import {createRoot} from 'react-dom/client'
+function App()
+{
+    return(
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+//ReactDOM.render(<App/>,document.getElementById('root'))
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
