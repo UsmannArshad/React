1c657d79f6e08104516ce98f240369d1459e0d64
UsmannArshad <usmanarshadbhatti480@gmail.com>
2022-06-08 19:52:44 +0500

    return multiple statements

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -9,14 +9,20 @@ function App()
         </div>
     )
 }
-function App2()
-{
-    return(
-        React.createElement('div',{className:'imclass'},React.createElement('h1',{},'Hello World'))
-    )
-}
+const App2=()=>   //same as app
+(        
+    React.createElement('div',{className:'imclass'},React.createElement('h1',{},'Hello World'))
+)
+const App3=()=>   //same as app
+(        
+    <>
+        <h1>Hello World</h1>
+        <h2>Hello Pakistan</h2>
+    </>
+)
 //ReactDOM.render(<App/>,document.getElementById('root'))  //not suported inr react18
 const container = document.getElementById('root');
 const root = createRoot(container);
 //root.render(<App tab="home" />); by returning in form of html
-root.render(<App2/>)
+//root.render(<App2/>)
+root.render(<App3/>)
