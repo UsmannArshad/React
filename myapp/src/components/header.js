f47c6d3648994d81c716a762af2a823065b60f1b
UsmannArshad <usmanarshadbhatti480@gmail.com>
2022-06-08 21:03:48 +0500

    default export vs named export

--- myapp/src/components/header.js
+++ myapp/src/components/header.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Check,{Check1} from "./news_list";
 function ShowHeader()
 {
+    function CalculateDate()
+    {
+        const date=new Date();     //if we want to do some computation as in .jsx we cant have more than 1 statement
+        return date.getFullYear();
+    }
     return(
         <>
         <h1>Header</h1>
+        <h2>Date is {CalculateDate()}</h2>
+        <h3><Check/></h3>
+        <h4><Check1/></h4>
         </>
     )
 }
---
+++ myapp/src/components/news_list.js
@@ -0,0 +1,8 @@
+import React from "react";
+function Check()
+{
+    return (<h1>Check</h1>)
+}
+const Check1=()=>(<h1>Check1</h1>)
+export default Check;
+export {Check1};
