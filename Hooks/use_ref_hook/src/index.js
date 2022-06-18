8584354f4cc8aa301575457ee0d336ff12f4ef34
UsmannArshad <usmanarshadbhatti480@gmail.com>
2022-06-18 19:13:02 +0500

    use_ref_hook p3

--- Hooks/use_ref_hook/src/index.js
+++ Hooks/use_ref_hook/src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {createRoot} from 'react-dom/client'
 import App from './app';
 import App1 from './components/app1'
+import App2 from './components/app2'
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<><App/><App1/></>)
+root.render(<><App/><App1/><App2/></>)
