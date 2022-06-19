f979f378a19867b00af4f357f335af269e96f108
UsmannArshad <usmanarshadbhatti480@gmail.com>
2022-06-19 15:41:30 +0500

    react basics done

--- Hooks/use_context_hook/src/child.js
+++ Hooks/use_context_hook/src/child.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 import GrandChild from './grandchild'
-const Child=()=>{
-return(
-    <>
-    <GrandChild/>
-    </>
-)
+import { myContext } from './context/context'
+class Child extends Component{    //We want to access context in this class
+    //static contextType=myContext  //method1
+    render()
+    {
+        //console.log(this.context)   //method1
+        return(
+            <>
+            <h2> From Child</h2>
+            <myContext.Consumer>
+                {
+                    context=>
+                    (
+                        (context.users.map((item)=>(
+                            <div key={item.id}>{item.name}</div>
+                        )))
+                    )
+                }
+            </myContext.Consumer>
+            <GrandChild/>         
+            </>
+        )
+    }
 } 
 export default Child
