d93fc36563b22bd8063a23a03e2213a490855c1f
UsmannArshad <usmanarshadbhatti480@gmail.com>
2022-06-20 13:58:14 +0500

    stage1 list shown

--- WhoPaysTheBill/whopaysbill/src/components/stage1.js
+++ WhoPaysTheBill/whopaysbill/src/components/stage1.js
@@ -1,10 +1,62 @@
-import React from 'react'
+import React, { createRef, useContext, useState } from 'react'
+import {Button,Form,Alert} from'react-bootstrap'
+import { MyContext } from '../context'
 const Stage1=()=>{
+    const text=createRef()
+    const context=useContext(MyContext)
+    console.log(context.state.players)
+    const [error,SetError]=useState([false,''])
+    const SubmitHandler=(e)=>{
+        e.preventDefault();
+        const check=ValidInput(text.current.value)
+        if(check===true)
+        {
+            context.AddPlayer(text.current.value)
+        }
+        else
+        {
+            console.log("nikal")
+        }
+    }
+    const ValidInput=(val)=>{
+        if(val===''){
+            SetError([true,'Sorry You need to add something'])
+            return false
+        }
+        if(val.length<=2)
+        {
+            SetError([true,'Name should be atleast of length 3'])
+            return false
+        }
+        SetError([false,'Name should be atleast of length 3'])
+        return true
+    }
     return (
-        <h1>
-            S1
-        </h1>
+        <>
+        { error[0] ? <Alert variant='danger'>
+        {error[1]}
+        </Alert>: null } 
+        <Form onSubmit={SubmitHandler} className='mt-2'>
+            <Form.Group>
+                <Form.Control type="text" placeholder='Add Player Name' name='player' ref={text} className='input_text mt-2'/>
+            </Form.Group>
+            <Button variant='primary' type='submit' className='addbtn mt-3'>Add</Button>
+        </Form>
+        {
+            context.state.players.length>0 ?
+            <>
+            <hr/>
+            <div>
+                <ul className='list-group'>
+                {context.state.players.map((item,i)=>(
+            <li key={i} className="list-group-item d-flex justify-content-between align-aitem-center list-group-item-action">{item} <span className="badge bg-danger" onClick={context.RemovePlayer}>x</span></li>))}
+                </ul>
+            </div>
+            </>
+            :null
+        }
+        </>
     );
-  }
-  
-  export default Stage1;
+}
+
+export default Stage1;
--- WhoPaysTheBill/whopaysbill/src/context/index.js
+++ WhoPaysTheBill/whopaysbill/src/context/index.js
@@ -9,10 +9,26 @@ class MyProvider extends Component{
             result:''
         }
     }
+    AddPlayer=(value)=>{
+        this.setState(({
+            players:[...this.state.players,value]
+        }))
+    }
+    RemovePlayer=(value)=>{
+        console.log(value)
+        let arr=this.state.players
+        let index=arr.indexOf(value)
+        arr.splice(index,1)
+        this.setState(({
+            players:arr
+        }))
+    }
     render(){
         return(
             <MyContext.Provider value={{
-                state:this.state
+                state:this.state,
+                AddPlayer:this.AddPlayer,
+                RemovePlayer:this.RemovePlayer
             }}>
                 {this.props.children}
             </MyContext.Provider>
